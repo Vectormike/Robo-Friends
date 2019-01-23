2ee9521133f55a764b63e8fcce1783432bf66fe7
Vectormike40 <victor.junior40@yahoo.com>
2019-01-23 16:30:28 +0100

    Parent component made

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots'
+
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+        
+        
+    }
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()) 
+        })
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        );
+    }
+    
+}
+
+export default App;
