ab887f6963750bf33c50103b28874c242836b243
Vectormike40 <victor.junior40@yahoo.com>
2019-01-24 16:45:05 +0100

    ..

--- src/CardList.js
+++ src/CardList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Card from './Card';
-import {robots} from './robots';
 
-const CardList = () => {
+
+const CardList = ({robots}) => {
     
     return ( 
         <div>
